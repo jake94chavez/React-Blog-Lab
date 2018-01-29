498762c8a1381033c9ebea6a91b5f32099555ce2
Jake Chavez <jake94chavez@gmail.com>
2018-01-29 15:43:14 -0600

    BLOG APP COMPLETE

--- src/Post.js
+++ src/Post.js
@@ -4,13 +4,26 @@ import './Post.css';
 import Comment from './Comment.js'
 
 class Post extends Component {
+  constructor (props) {
+    super()
+    this.state = {
+      body: 'Bears, Beets, Battlestar Galactica'
+    }
+  }
+  handleClick (e) {
+    let newBody = prompt('Change the body')
+    this.setState({
+      body: newBody
+    })
+  }
   render() {
     return (
       <div>
         <h1>{this.props.title}</h1>
         <p>By {this.props.author}</p>
         <div>
-          <p>{this.props.body}</p>
+          <p>{this.state.body}</p>
+          <button onClick={(e) => this.handleClick(e)}>click to render!</button>
         </div>
         <h3>Comments:</h3>
         {/* Render Comment component, passing in data */}
@@ -21,5 +34,4 @@ class Post extends Component {
     );
   }
 }
-
 export default Post;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import Post from './Post';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-	<Post title={'Bears'} author={'Dwight Schrute'} body={'Bears, Beats, Battlestar Gallactica'} comments={['Identity theft is not a joke Jim!', 'Millions of families suffer every year!']} />, 
+	<Post title={'BBB'} author={'Dwight Schrute'} body={'Bears, Beats, Battlestar Gallactica'} comments={['Identity theft is not a joke Jim!', 'Millions of families suffer every year!']} />, 
 	document.getElementById('root'));
 registerServiceWorker();
