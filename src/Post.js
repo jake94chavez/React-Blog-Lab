19bf3532367cd1e166c4093450a6f60b0c2cb05c
Jake Chavez <jake94chavez@gmail.com>
2018-01-29 15:27:29 -0600

    completed step 3

--- src/Post.js
+++ src/Post.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './Post.css';
+import Comment from './Comment.js'
 
 class Post extends Component {
   render() {
     return (
-      <div className="Post">
-        <header className="Post-header">
-          <img src={logo} className="Post-logo" alt="logo" />
-          <h1 className="Post-title">Welcome to React</h1>
-        </header>
-        <p className="Post-intro">
-          To get started, edit <code>src/Post.js</code> and save to reload.
-        </p>
-        <p>{this.props.title}</p>
-        <p>{this.props.author}</p>
-        <p>{this.props.body}</p>
-        <p>{this.props.comments}</p>
+      <div>
+        <h1>{this.props.title}</h1>
+        <p>By {this.props.author}</p>
+        <div>
+          <p>{this.props.body}</p>
+        </div>
+        <h3>Comments:</h3>
+        {/* Render Comment component, passing in data */}
+        {this.props.comments.map((comment) => {
+          return <Comment body={comment} />;
+        })}
       </div>
     );
   }
